9ee838f6a34a62955a6bc932a2ba58a62fa569b8
SergeiRadchenkov <radchenkovsa@yandex.ru>
2024-10-05 03:56:06 +0300

    Seminar 5

---
+++ Seminars/Seminar_5/task_07.py
@@ -0,0 +1,24 @@
+'''
+Создайте функцию генератор.
+Функция генерирует N простых чисел начиная с 2.
+Для проверки числа на простоту используйте правило:
+"Число является простым, если делится нацело только на еденицу и на себя". 
+'''
+
+
+def is_prime(num: int):
+    if num % 2 == 0 and num != 2:
+        return False
+    for div in range(3, int(num**0.5) + 1):
+        if num % div == 0:
+            return False
+    return True
+
+
+def prime_generator(n: int):
+    for number in range(2, n + 1):
+        if is_prime(number):
+            yield number
+
+
+print(*prime_generator(80))
