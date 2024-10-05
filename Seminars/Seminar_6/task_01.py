bb64aa8e51c1f1ff19ea37e2ff35d3d7e346d0d1
SergeiRadchenkov <radchenkovsa@yandex.ru>
2024-10-05 19:27:39 +0300

    Seminar 6

---
+++ Seminars/Seminar_6/task_01.py
@@ -0,0 +1,11 @@
+'''
+Вспомните какие модули вы уже проходили на курсе.
+Создайте файл, в котором вы импортируете встроенные в
+модуль функции под псевдонимами. (3-7 строк импорта).
+'''
+
+from random import randint as rnd
+from decimal import Decimal as dec
+from math import sqrt as sq
+from fractions import Fraction as fr
+from sys import argv as ar
